8d5c7f61673e5daf1f8847e7fbaa5c2aacf2b996
parthivhere-hue <parthivhere@gmail.com>
2026-09-07 17:26:24 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+pins
+pins
+if (true) {
+    input.onButtonPressed(Button.A, function() {
+        
+music.play(music.stringPlayable("", 120), music.PlaybackMode.UntilDone)    })
+    
+} else
+for (let i = 8; i < 5; i++) {
+    
+}
+
+ 
+led.plot(7, 9)
